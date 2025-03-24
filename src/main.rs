8c15979514b87fbc9e95d924e1131164d81eb899
mcurrie99 <45800774+mcurrie99@users.noreply.github.com>
2025-03-23 21:16:47 -0400

    Last of initial commits

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use currspice_mods as mods;
 
-#[cfg(not(target_os = "windows"))]
-compile_error!("This application only supports Windows");
+// #[cfg(not(target_os = "windows"))]
+// compile_error!("This application only supports Windows");
 
 fn main() {
     let test = mods::tools::guess_minecraft_dir().unwrap();
