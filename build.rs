d9e0d0d1b120559b3ec2d6cce27ae6de589aae21
mcurrie99 <45800774+mcurrie99@users.noreply.github.com>
2025-05-31 18:33:34 -0400

    Icon addition is working

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut res = winres::WindowsResource::new();
+
+    // Path to your .ico file (must exist)
+    res.set_icon("icon.ico");
+
+    res.compile().expect("Failed to compile resources");
+}
